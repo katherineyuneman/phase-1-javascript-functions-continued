76461016643faca14407ac7832a12244252e3a11
KatherineYuneman <kyuneman@gmail.com>
2021-10-16 22:20:23 -0400

    commit tree 243826276009931454b0e2ba596506d649c2cfd8

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+console.log(saturdayFun());
+console.log(mondayWork());
+
+
+
+function wrapAdjective(emphatic = "*") {
+    return function(adjective = "special"){
+        return (`You are ${emphatic}${adjective}${emphatic}!`);
+    };
+};
+console.log(wrapAdjective()());
+
+
+
